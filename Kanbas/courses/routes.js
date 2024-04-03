f8a6c054364035b5fc9e0177f83b21489808a734
leo.senai <LSenai@gmail.com>
2024-04-03 01:53:59 -0400

    bedtime

--- Kanbas/courses/routes.js
+++ Kanbas/courses/routes.js
@@ -9,6 +9,32 @@ export default function CourseRoutes(app) {
 
     const API_COURSES = "/api/courses";
 
+    app.get("/api/courses/:id", (req, res) => {
+        const { id } = req.params;
+        const course = Database.courses
+          .find((c) => c._id === id);
+        if (!course) {
+          res.status(404).send("Course not found");
+          return;
+        }
+        res.send(course);
+      });
+    
+
+    app.put(`${API_COURSES}/:id`, (req, res) => {
+        const { id } = req.params;
+        const course = req.body;
+        Database.courses = Database.courses.map((c) => c.id === id ? {...c, ...course} : c);
+        res.sendStatus(204);
+    });
+
+    app.delete(`${API_COURSES}/:id`, (req, res) => {
+        const { id } = req.params;
+        Database.courses = Database.courses.filter((c) => c._id !== id);
+        res.sendStatus(204)
+    });
+
+    // Create new course
     app.post(`${API_COURSES}`, (req, res) => {
         const course = {...req.body, 
             id: new Date().getTime().toString() };
@@ -20,4 +46,5 @@ export default function CourseRoutes(app) {
         const courses = Database.courses;
         res.send(courses)
     })
+
 }
