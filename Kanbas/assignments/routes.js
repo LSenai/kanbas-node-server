22de33b9ef8285c6593a33705a217e1706b05ef9
leo.senai <LSenai@gmail.com>
2024-04-04 22:13:30 -0400

    finished assignments. could be better, but meets requirements

--- Kanbas/assignments/routes.js
+++ Kanbas/assignments/routes.js
@@ -22,7 +22,24 @@ function AssignmentRoutes(app) {
         res.send(assignments);  
     });
 
+    // UPDATE
+    app.put("/api/assignments/:aid", (req, res) => {
+        const { aid } = req.params;
+        const assignmentIndex = db.assignments.findIndex(
+            (a) => a._id === aid);
+        db.assignments[assignmentIndex] = {
+            ...db.assignments[assignmentIndex], 
+            ...req.body
+        };
+        res.sendStatus(204);
+    });
 
+    // DELETE
+    app.delete("/api/assignments/:aid", (req, res) => {
+        const { aid } = req.params;
+        db.assignments = db.assignments.filter((a) => a._id !== aid);
+        res.sendStatus(200);
+    });
 
 }
 export default AssignmentRoutes;
